bfb4154880e3731b3f05888232a49f556c36be15
RP231013 <231013@virtualwindow.co.za>
2024-05-16 14:28:40 +0200

    add forms

--- pawprints/src/App.js
+++ pawprints/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -12,40 +12,30 @@ import ContactUs from "./routes/ContactUs/ContactUs";
 import AdminHome from "./routes/Admin/AdminHome";
 import Approvals from "./routes/Admin/Approvals";
 import Users from "./routes/Admin/Users";
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Main />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/petmatching" element={<PetMatching />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/admin/*" element={
+            <div className="admin-container">
+              <VerticalNav />
+              <Routes>
+                <Route path="home" element={<AdminHome />} />
+                <Route path="approvals" element={<Approvals />} />
+                <Route path="users" element={<Users />} />
+              </Routes>
+            </div>
+          } />
+        </Routes>
       </BrowserRouter>
     </div>
-  );
-}
-
-function Main() {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
-
-  return (
-    <>
-      {!isAdminRoute && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/petmatching" element={<PetMatching />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/admin/*" element={
-          <div className="admin-container">
-            <VerticalNav />
-            <Routes>
-              <Route path="home" element={<AdminHome />} />
-              <Route path="approvals" element={<Approvals />} />
-              <Route path="users" element={<Users />} />
-            </Routes>
-          </div>
-        } />
-      </Routes>
-    </>
   );
 }
 
