a16e11232184e73986c13454e9b1341ab443e5e6
RP231013 <231013@virtualwindow.co.za>
2024-05-16 10:42:20 +0200

    bug fix

--- pawprints/client/src/App.js
+++ pawprints/client/src/App.js
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> parent of 8e91de0 (navbar-completed)
 
 import Navbar from "./components/Navbar/Navbar";
 
@@ -13,6 +18,7 @@ import ContactUs from "./routes/ContactUs/ContactUs";
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -22,6 +28,22 @@ function App() {
           {/* Add more routes as needed */}
         </Routes>
       </BrowserRouter>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of 8e91de0 (navbar-completed)
     </div>
   );
 }
