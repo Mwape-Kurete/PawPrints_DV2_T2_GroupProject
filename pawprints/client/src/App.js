68a51cbd86c7f06cd26f0d4744ae790115594e3a
RP231013 <231013@virtualwindow.co.za>
2024-05-26 17:49:50 +0200

    Filing dilemma sorted

---
+++ pawprints/client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+import Navbar from "./components/Navbar/Navbar";
+
+import Home from "./routes/Home/Home";
+import PetMatching from "./routes/PetMatching/PetMatching";
+import ContactUs from "./routes/ContactUs/ContactUs";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/petmatching" element={<PetMatching />} />
+          <Route path="/contact" element={<ContactUs />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
